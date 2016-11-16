fee4ce5b65118cedc04441905ea035bc5173c0db
leindecker <leindecker.guilherme@gmail.com>
2016-11-16 15:10:14 -0200

    Scenarios improvements: Created a method in Utils.js to perfom a math calc.

--- framework/utils.js
+++ framework/utils.js
@@ -1,7 +1,26 @@
 var Utils = function() {
 
-  this.perfomMathCalc = function(valueA, valueB) {
-    var result = valueA + valueB;
+  this.perfomMathCalc = function(valueA, valueB, mathOperator) {
+    var result;
+    switch (mathOperator) {
+      case "ADDITION":
+        result = valueA + valueB;
+        break;
+      case "DIVISION":
+        result = valueA / valueB;
+        break;
+      case "MODULO":
+        result = valueA % valueB;
+        break;
+      case "MULTIPLICATION":
+        result = valueA * valueB;
+        break;
+      case "SUBTRACTION":
+        result = valueA - valueB;
+        break;
+      default:
+
+    }
     return result.toString();
   }
 
--- pages/calculator_page.js
+++ pages/calculator_page.js
@@ -1,41 +1,36 @@
-        var CalculatorPage = function() {
-          this.inputCalculator_1 = element(by.model('first'));
-          this.inputCalculator_2 = element(by.model('second'));
-          this.latest_result = element(by.binding('latest'));
-          this.mathResult = element(by.className('ng-binding'));
-          this.selectOperation = element(by.model('operator'));
-          this.history = element.all(by.repeater('result in memory'));
-          this.btnGo = element(by.id('gobutton'));
+var CalculatorPage = function() {
+  this.inputCalculator_1 = element(by.model('first'));
+  this.inputCalculator_2 = element(by.model('second'));
+  this.latest_result = element(by.binding('latest'));
+  this.mathResult = element(by.className('ng-binding'));
+  this.selectOperation = element(by.model('operator'));
+  this.history = element.all(by.repeater('result in memory'));
+  this.btnGo = element(by.id('gobutton'));
 
-          this.navigate = function() {
-            browser.get(browser.params.page.appUrl);
-            browser.driver.manage().window().maximize();
-          };
+  this.navigate = function() {
+    browser.get(browser.params.page.appUrl);
+    browser.driver.manage().window().maximize();
+  };
 
-          this.sendInputsToPerfolMathCalc = function(valueA, valueB, operator) {
-            this.selectMathOperation(operator);
-            this.inputCalculator_1.sendKeys(valueA);
-            this.inputCalculator_2.sendKeys(valueB);
-            this.btnGo.click();
-          };
+  this.sendInputsToPerfolMathCalc = function(valueA, valueB, operator) {
+    this.selectMathOperation(operator);
+    this.inputCalculator_1.sendKeys(valueA);
+    this.inputCalculator_2.sendKeys(valueB);
+    this.btnGo.click();
+  };
 
-          this.selectMathOperation = function(operator) {
-            element(by.model('operator')).$('[value="' + operator + '"]').click();
-          };
+  this.selectMathOperation = function(operator) {
+    element(by.model('operator')).$('[value="' + operator + '"]').click();
+  };
 
-          //Deprecated
-          this.checkResult = function(result) {
-            expect(this.latest_result.getText()).toEqual(result);
-          };
+  this.checkMathHistory = function(count) {
+    expect(this.history.count()).toEqual(count);
+  };
 
-          this.checkMathHistory = function(count) {
-            expect(this.history.count()).toEqual(count);
-          };
+  this.checkMathResult = function(mathResult) {
+    expect(this.latest_result.getText()).toEqual(mathResult);
+  };
 
-          this.checkMathResult = function(exptectMathResult) {
-            expect(this.mathResult.getText()).toEqual(exptectMathResult);
-          };
+};
 
-        };
-
-        module.exports = new CalculatorPage();
+module.exports = new CalculatorPage();
